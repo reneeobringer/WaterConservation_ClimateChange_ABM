aae5d5aee9897f9c6882a3eae1895cb47159e989
Renee Obringer <obringer@psu.edu>
2024-11-20 16:13:29 -0500

    Add Files

---
+++ jobscript.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+./NetLogo-6.2.2/netlogo-headless.sh \
+	--model "./climatechangeanalysis.nlogo" \
+	--experiment "climate-models" \
+	--spreadsheet exp_archetypes_all_scenarios_allcities.csv 
+
+
